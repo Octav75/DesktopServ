62f5cfc3d7cbb1050875fc2016daaf81d72d41a2
Octav75 <OctavSwDev@gmail.com>
2019-05-20 16:27:17 +0300

    Impartire

--- $Recaptulare/w3resource/Basic_I/src/ProductOfTwoNumbers.java
+++ $Recaptulare/w3resource/Basic_I/src/ProductOfTwoNumbers.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ProductOfTwoNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduceti primul numar");
+        System.out.println("Introduceti primul numar a =");
         int a = input.nextInt();
-        System.out.println("Introduceti al doilea numar");
+        System.out.println("Introduceti al doilea numar b =");
         int b = input.nextInt();
         int prod = a * b;
-        System.out.println("Produsul este " + prod);
+        System.out.println(a + " x " + b + " = " + prod);
     }
 }
