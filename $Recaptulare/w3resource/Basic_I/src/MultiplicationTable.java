8ccbf473d204049673b3971a07304e4a35b28f8f
Octav75 <OctavSwDev@gmail.com>
2019-05-24 10:53:19 +0300

    Tabla inmultirii

--- $Recaptulare/w3resource/Basic_I/src/MultiplicationTable.java
+++ $Recaptulare/w3resource/Basic_I/src/MultiplicationTable.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner newNumber = new Scanner(System.in);
+        System.out.println("Introduceti numarul pe care doriti sa il multiplicati : ");
+        int a = newNumber.nextInt();
+        int i;
+        int multy = 0;
+        for (i = 1; i <= 10; i++) {
+            multy = a * i;
+        }
+        System.out.println ("Care este " + multy);
+    }
 }
